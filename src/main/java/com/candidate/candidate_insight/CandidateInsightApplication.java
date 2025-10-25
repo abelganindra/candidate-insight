38a22997a3b0378d0c6a73d2d526441589b97f59
aganindra <abelganindra.work@gmail.com>
2025-10-25 20:55:56 +0700

    first commit

---
+++ src/main/java/com/candidate/candidate_insight/CandidateInsightApplication.java
@@ -0,0 +1,13 @@
+package com.candidate.candidate_insight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateInsightApplication.class, args);
+	}
+
+}
